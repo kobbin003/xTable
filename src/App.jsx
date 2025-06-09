1e6d6ae400ed82ab31a11615b436c066d942d5d2
kobbin003 <dkfeto@gmail.com>
2025-06-09 12:33:04 +0530

    xtable done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
 import "./App.css";
+import { data } from "./data/data";
 
 function App() {
-	return <></>;
+	const [sortedData, setSortedData] = useState(data);
+
+	const handleSortByViews = () => {
+		setSortedData((prev) => {
+			return [
+				...prev.sort((a, b) => {
+					return b.views - a.views; // descending order
+				}),
+			];
+		});
+	};
+
+	const handleSortBydate = () => {
+		setSortedData((prev) => {
+			return [
+				...prev.sort((a, b) => {
+					return new Date(b.date) - new Date(a.date); // descending order
+				}),
+			];
+		});
+	};
+
+	return (
+		<>
+			<div>
+				<button onClick={handleSortBydate}>Sort by Date</button>
+				<button onClick={handleSortByViews}>Sort by Views</button>
+			</div>
+			<table>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Views</th>
+						<th>Country</th>
+					</tr>
+				</thead>
+				<tbody>
+					{sortedData.map(({ date, views, article }, index) => (
+						<tr key={index}>
+							<td>{date}</td>
+							<td>{views}</td>
+							<td>{article}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	);
 }
 
 export default App;
--- src/data/data.js
+++ src/data/data.js
@@ -6,6 +6,5 @@ export const data = [
 	{ date: "2023-09-02", views: 150, article: "Article 2" },
 
 	{ date: "2023-09-02", views: 120, article: "Article 3" },
-
 	{ date: "2020-09-03", views: 200, article: "Article 4" },
 ];
